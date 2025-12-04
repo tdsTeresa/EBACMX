c2819f0313cae619fefe364703cb6ac8b062b9a3
ACERTERE\einzw <einzwei3@hotmail.com>
2025-12-04 13:19:37 -0600

    Archivo Cálculo1.py para trabajar con nuevo branch

---
+++ Cálculo1.py
@@ -0,0 +1,12 @@
+# Archivo Python de prueba
+# Se añadió la variable 'c'
+a= 10
+b= 30
+c= 30 * 10 * 5
+print('resultado= ', c)
+
+# Cambiado el factor 102 según requerimiento del negocio
+# Cambiado el factor 110 según Junta Control
+
+d= c * 110
+print('Resultado total= ', d)
