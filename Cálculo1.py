1839c5b1a902525025e7df8224eac5b28ea26ede
Teresa Desales <einzwei3@hotmail.com>
2025-12-04 15:42:12 -0600

    Update Cálculo1.py
    Se añade la variable 'f'

--- Cálculo1.py
+++ Cálculo1.py
@@ -1,10 +1,13 @@
 # Archivo Python de prueba
 # Se añadió la variable 'c'
 # Nueva variable 'e'
+# Se añade la variable 'f' como factor de tendencia
 a= 10
 b= 30
 e= 1.1
-c= 30 * 10 * 5 / e
+f= 5
+
+c= 30 * 10 * 5 / e * (1 + f / 100)
 print('resultado= ', c)
 
 # Cambiado el factor 102 según requerimiento del negocio
